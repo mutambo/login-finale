13754681f84a69311b596d8b583ccf3aa9ef6f19
Wwashy <52789151+Wwashy@users.noreply.github.com>
2020-07-13 00:35:15 +0300

    initial commit

---
+++ public/postSelecta.js
@@ -0,0 +1,6 @@
+$(document).ready(()=>{
+    //hii ndio JQuery code itaswitch buttons for either login or sign up
+    $('#signup-button').click(()=>{
+        $('#data-form').attr("action","/signup")
+    })
+});
---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const mysql = require('mysql')
+const bodyparser = require('body-parser');
+
+const app = express();
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'washy',
+    password: 'Hellen@1999',
+    database: 'users'
+})
+connection.connect((err) => {
+    if (err) throw err
+    console.log('connected successfully')
+})
+
+app.use(bodyparser.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.set('port', (process.env.PORT || 4000))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+app.post('/signup', (req, res) => {
+    let sql = "INSERT INTO users(u_email,u_password) VALUES('" + req.body.email + "','" + req.body.password + "');"
+    connection.query(sql, (err, results) => {
+        if (err) throw err
+        res.send("<a href ='/'>click here to login</a>")
+    })
+})
+
+app.post('/login', (req, res) => {
+    let sql = "SELECT * FROM users WHERE u_email='"+req.body.email+"' AND u_password='"+req.body.password+"';"
+    connection.query(sql,(err, result) => {
+        if (err) throw err;
+        console.log(result[0].u_email);
+        if (result !== '') {
+            let email = result[0].u_email;
+            let password = result[0].u_password;
+
+            if (req.body.email == email && req.body.password == password) {
+                res.sendFile(__dirname + '/welcome.html')
+            } else {
+                res.sendFile(__dirname + '/index.html')
+            }
+        }
+    })
+
+})
+
+app.listen(app.get('port'), () => {
+    console.log("listening to port 4000", app.get('port'));
+})
